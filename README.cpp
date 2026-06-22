a61a662149960514710bb0cab7319f722c695071
Avinash Shukla <221488610+Avinashukla960@users.noreply.github.com>
2026-06-22 23:55:57 +0530

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -3,6 +3,7 @@
 
 Physics Hub is a simple, interactive, and student-friendly web platform designed to make learning physics concepts easier and more engaging. It provides well-organized topics, clear explanations, and a clean interface to help students understand fundamental physics concepts efficiently.
 
+  
 🔗 Live Demo: Visit Physics Hub - https://avinashukla960.github.io/Physics-Hub/
 
 🚀 Features
